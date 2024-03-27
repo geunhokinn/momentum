df7256fb20a962d9a8aa5ef1b7183b225fc60a40
geunhokinn <gghh1220@gmail.com>
2024-03-27 12:07:34 +0900

    feat: 체크 박스 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -10,6 +10,20 @@ function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+function checkToDo(event) {
+    const checkBox = event.target;
+    const li = checkBox.parentElement;
+    const checkedToDo = toDos.find(todo => todo.id === parseInt(li.id));
+    checkedToDo.complete = checkBox.checked;
+    saveToDos();
+    const span = li.querySelector("span");
+    if(checkedToDo.complete){
+        span.classList.add("complete");
+    }else{
+        span.classList.remove("complete");
+    }
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
@@ -20,11 +34,19 @@ function deleteToDo(event) {
 function paintToDo(newToDo) {
     const li = document.createElement("li");
     li.id = newToDo.id;
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.addEventListener("change", checkToDo);
     const span = document.createElement("span");
     span.innerText = newToDo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo);
+    if(newToDo.complete) {
+        checkBox.checked = true;
+        span.classList.add("complete");
+    }
+    li.appendChild(checkBox);
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
@@ -37,6 +59,7 @@ function handleToDoSubmit(event) {
     const newTodoObj = {
         text: newToDo,
         id: Date.now(),
+        complete: false,
     }
     toDos.push(newTodoObj);
     paintToDo(newTodoObj);
